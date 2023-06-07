c2c4e0e25c1773d408959900cff428468619266a
Jean-Yanis Jeffroy <jean-yanis.jeffroy@epitech.eu>
2023-06-07 16:32:31 +0200

    refactor(main) proper implementation of SYST

--- src/client/commands/syst.c
+++ src/client/commands/syst.c
@@ -10,5 +10,14 @@
 int syst(client_t *client, char *arg)
 {
     (void)arg;
-    return (dprintf(client->fd, "215 UNIX Type: L8\r\n"), 0);
+
+    #if defined(_WIN32)
+        return (dprintf(client->fd, "215 Windows_NT Type: L8\r\n"), 0);
+    #elif defined(__linux__)
+        return (dprintf(client->fd, "215 UNIX Type: L8\r\n"), 0);
+    #elif defined(__APPLE__) && defined(__MACH__)
+        return (dprintf(client->fd, "215 MacOS Type: L8\r\n"), 0);
+    #else
+        return (dprintf(client->fd, "215 Unknown Type: L8\r\n"), 0);
+    #endif
 }
