900f8ead334f537e665b6763bccc5a961c443e1d
Jean-Yanis Jeffroy <jean-yanis.jeffroy@epitech.eu>
2024-02-12 13:06:47 +0100

    feat(login): need to have correct credentials

--- src/client/commands/pass.c
+++ src/client/commands/pass.c
@@ -9,9 +9,20 @@
 
 int pass(client_t *client, char *arg)
 {
+    int isValidPassword = 0;
+
     arg = arg ? arg : "";
+    for (int i = 0; default_users[i].username != NULL; i++) {
+        if (strcmp(default_users[i].username, client->username) == 0
+            && strcmp(default_users[i].password, arg) == 0) {
+            isValidPassword = 1;
+            break;
+        }
+    }
+    if (!isValidPassword)
+        return dprintf(client->fd, "530 Login incorrect.\r\n"), -1;
     if (client->password)
         free(client->password);
     client->password = strdup(arg);
-    return (dprintf(client->fd, "230 User logged in, proceed.\r\n"), 0);
+    return dprintf(client->fd, "230 User logged in, proceed.\r\n"), 0;
 }
--- src/client/commands/user.c
+++ src/client/commands/user.c
@@ -11,8 +11,12 @@ int user(client_t *client, char *arg)
 {
     if (!arg)
         return (dprintf(client->fd, "xxx Error (RFC compliant)\r\n"), 1);
-    if (client->username)
-        free(client->username);
-    client->username = strdup(arg);
-    return (dprintf(client->fd, "331 User name okay, need password.\r\n"), 0);
+    for (int i = 0; default_users[i].username != NULL; i++) {
+        if (strcmp(arg, default_users[i].username) == 0) {
+            client->username = strdup(arg);
+            return (dprintf(client->fd,
+                "331 User name okay, need password.\r\n"), 0);
+        }
+    }
+    return (dprintf(client->fd, "xxx Error: Invalid user\r\n"), 1);
 }
