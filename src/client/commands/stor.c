5aaee1f0edf05ccbcca31d7ffd25a49233f6b524
neo-jgrec <jean-yanis.jeffroy@epitech.eu>
2024-02-14 01:01:53 +0100

    refactor(stor): make it without using fork

--- src/client/commands/stor.c
+++ src/client/commands/stor.c
@@ -11,32 +11,35 @@ static const char *reply_start =
 "150 Opening BINARY mode data connection for file transfer.\r\n";
 static const char *reply_complete = "226 Transfer complete.\r\n";
 
-static void do_stor(client_t *client, FILE *file)
+static void handle_data_fd(client_t *client, FILE *file)
 {
     char buffer[1024];
     ssize_t bytes = tcp_recv(client->data_fd, buffer, sizeof(buffer));
 
-    for (; bytes > 0;
-        bytes = tcp_recv(client->data_fd, buffer, sizeof(buffer)))
+    while (bytes > 0) {
         fwrite(buffer, 1, bytes, file);
+        bytes = tcp_recv(client->data_fd, buffer, sizeof(buffer));
+    }
     fclose(file);
     tcp_send(client->fd, reply_complete, strlen(reply_complete));
     close(client->data_fd);
-    exit(0);
 }
 
-static int stor_fork(client_t *client, FILE *file)
+static int stor_select(client_t *client, FILE *file)
 {
-    pid_t pid = fork();
+    fd_set read_fds;
+    int max_fd;
+    int ready;
 
-    if (pid == -1) {
+    tcp_fd_set_init(&read_fds, client->data_fd);
+    max_fd = client->data_fd + 1;
+    ready = tcp_select(max_fd, &read_fds);
+    if (!ERROR_HANDLING(ready, "stor : select")) {
         fclose(file);
         return 1;
-    } else if (pid > 0) {
-        fclose(file);
-        return 0;
-    } else
-        do_stor(client, file);
+    }
+    if (FD_ISSET(client->data_fd, &read_fds))
+        handle_data_fd(client, file);
     return 0;
 }
 
@@ -48,7 +51,7 @@ static int stor_active(client_t *client, char *arg)
     if (!ERROR_HANDLING(file, "stor : fopen"))
         return 1;
     tcp_send(client->fd, reply_start, strlen(reply_start));
-    if (stor_fork(client, file))
+    if (stor_select(client, file))
         return 1;
     return 0;
 }
@@ -65,11 +68,11 @@ static int stor_passive(client_t *client, char *arg)
     tcp_send(client->fd, reply_start, strlen(reply_start));
     client->data_fd = accept(client->data_fd,
         (struct sockaddr *)&client_addr, &addrlen);
-    if (client->data_fd == -1) {
+    if (!ERROR_HANDLING(client->data_fd, "stor : accept")) {
         fclose(file);
         return 1;
     }
-    if (stor_fork(client, file))
+    if (stor_select(client, file))
         return 1;
     return 0;
 }
