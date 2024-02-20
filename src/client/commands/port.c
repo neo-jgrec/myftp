bda6f0c22cee147d55eb0953a29f9c10016dc67d
Jean-Yanis Jeffroy <jean-yanis.jeffroy@epitech.eu>
2024-02-20 15:45:22 +0100

    fix(port): handle when no args

--- src/client/commands/port.c
+++ src/client/commands/port.c
@@ -37,18 +37,19 @@ int port(client_t *client, char *arg)
 {
     int data_socket = 0;
     char ip[16];
-    struct ip_data_struct ip_data = create_ip_data_struct();
+    struct ip_data_struct ip_d = create_ip_data_struct();
 
-    sscanf(arg, "%u,%u,%u,%u,%u,%u", &ip_data.h1, &ip_data.h2,
-        &ip_data.h3, &ip_data.h4, &ip_data.p1, &ip_data.p2);
-    ip_data.port = ip_data.p1 * 256 + ip_data.p2;
-    snprintf(ip, 16, "%u.%u.%u.%u", ip_data.h1,
-        ip_data.h2, ip_data.h3, ip_data.h4);
-    data_socket = tcp_connect(ip, ip_data.port);
+    if (!ERROR_HANDLING(arg, "port : arg"))
+        return 1;
+    sscanf(arg, "%u,%u,%u,%u,%u,%u", &ip_d.h1, &ip_d.h2,
+        &ip_d.h3, &ip_d.h4, &ip_d.p1, &ip_d.p2);
+    ip_d.port = ip_d.p1 * 256 + ip_d.p2;
+    snprintf(ip, 16, "%u.%u.%u.%u", ip_d.h1, ip_d.h2, ip_d.h3, ip_d.h4);
+    data_socket = tcp_connect(ip, ip_d.port);
     if (!ERROR_HANDLING(data_socket, "port : tcp_connect"))
         return 1;
     client->transfer = ACTIVE_TRANSFER;
-    client->data_port = ip_data.port;
+    client->data_port = ip_d.port;
     client->data_fd = data_socket;
     if (!ERROR_HANDLING((int)tcp_send(client->fd, reply,
         strlen(reply)), "port : tcp_send"))
