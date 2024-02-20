4469f52635bee867b2bb172185d76f0da6f775ad
neo-jgrec <jean-yanis.jeffroy@epitech.eu>
2024-02-20 19:42:34 +0100

    tests(tcp_basics): redirect all std

--- tests/tcp_basics.c
+++ tests/tcp_basics.c
@@ -6,23 +6,30 @@
 */
 
 #include <criterion/criterion.h>
+#include <criterion/redirect.h>
 #include "tcp_lib.h"
 
-Test(tcp_lib, test_tcp_connect)
+void redirect_all_std(void)
+{
+    cr_redirect_stdout();
+    cr_redirect_stderr();
+}
+
+Test(tcp_lib, test_tcp_connect, .init = redirect_all_std)
 {
     int sockfd = tcp_connect("127.0.0.1", 8080);
     cr_assert(sockfd < 0, "connect error: Connection refused");
     close(sockfd);
 }
 
-Test(tcp_lib, test_tcp_listen)
+Test(tcp_lib, test_tcp_listen, .init = redirect_all_std)
 {
     int sockfd = tcp_listen(8080, 5);
     cr_assert(sockfd >= 0, "Failed to set up server");
     close(sockfd);
 }
 
-Test(tcp_lib, test_tcp_accept)
+Test(tcp_lib, test_tcp_accept, .init = redirect_all_std)
 {
     int server_sockfd = tcp_listen(8080, 5);
     cr_assert(server_sockfd >= 0, "Failed to set up server");
@@ -38,7 +45,7 @@ Test(tcp_lib, test_tcp_accept)
     close(server_sockfd);
 }
 
-Test(tcp_lib, test_tcp_send_recv)
+Test(tcp_lib, test_tcp_send_recv, .init = redirect_all_std)
 {
     const char *msg = "Hello, server!";
     char buffer[1024];
