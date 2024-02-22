6ff969f53a83888a2a18058451c1a59386810bb3
Jean-Yanis Jeffroy <jean-yanis.jeffroy@epitech.eu>
2024-02-22 16:43:05 +0100

    fix(delete)

--- src/client/commands/dele.c
+++ src/client/commands/dele.c
@@ -12,21 +12,16 @@ int dele(client_t *client, char *arg)
     char *path = NULL;
     char reply[] = "250 Requested file action okay, completed.\r\n";
 
-    if (client->username == NULL) {
-        dprintf(client->fd, "530 Please login with USER and PASS.\r\n");
-        return 0;
-    }
     if (arg == NULL) {
         dprintf(client->fd, "550 Failed to delete file.\r\n");
         return 0;
     }
-    asprintf(&path, "rm -f %s", arg);
-    if (system(path) == -1) {
+    asprintf(&path, "%s/%s", getcwd(NULL, 0), arg);
+    int ret = remove(path);
+    if (ret == 0) {
+        tcp_send(client->fd, reply, strlen(reply));
+    } else {
         dprintf(client->fd, "550 Failed to delete file.\r\n");
-        free(path);
-        return 0;
     }
-    dprintf(client->fd, "%s", reply);
-    free(path);
     return 0;
 }
