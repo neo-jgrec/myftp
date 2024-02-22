e78044db6dbc9c6d44e4afbea27cb84323bfc6f8
Jean-Yanis Jeffroy <jean-yanis.jeffroy@epitech.eu>
2024-02-22 14:11:12 +0100

    fix(user): compliance rfc

--- src/client/commands/user.c
+++ src/client/commands/user.c
@@ -12,12 +12,7 @@ int user(client_t *client, char *arg)
     if (!arg)
         return (dprintf(client->fd,
             "501 Syntax error in parameters or arguments.\r\n"), 1);
-    for (int i = 0; default_users[i].username != NULL; i++) {
-        if (strcmp(arg, default_users[i].username) == 0) {
-            client->username = strdup(arg);
-            return (dprintf(client->fd,
-                "331 User name okay, need password.\r\n"), 0);
-        }
-    }
-    return (dprintf(client->fd, "530 Not logged in.\r\n"), 1);
+    client->username = strdup(arg);
+    dprintf(client->fd, "331 User name okay, need password.\r\n");
+    return 0;
 }
